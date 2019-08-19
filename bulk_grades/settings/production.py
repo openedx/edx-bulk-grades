513da1b56ae13f0678820501a0a26406231cec8c
atesker <andrewtr@gmail.com>
2019-08-19 16:17:24 -0400

    update production url for intervention
    newline

--- bulk_grades/settings/production.py
+++ bulk_grades/settings/production.py
@@ -9,5 +9,8 @@ def plugin_settings(settings):
     Injects local settings into django settings
     """
     env_tokens = getattr(settings, 'ENV_TOKENS', {})
-    if env_tokens.get('ANALYTICS_API_CLIENT'):
-        settings.ANALYTICS_API_CLIENT = env_tokens['ANALYTICS_API_CLIENT']
+    auth_tokens = getattr(settings, 'AUTH_TOKENS', {})
+    if env_tokens.get('ANALYTICS_API_URL'):
+        settings.ANALYTICS_API_CLIENT['url'] = env_tokens['ANALYTICS_API_URL']
+    if auth_tokens.get('ANALYTICS_API_KEY'):
+        settings.ANALYTICS_API_CLIENT['token'] = auth_tokens['ANALYTICS_API_KEY']
