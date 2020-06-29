a88a654013011cf9e08fc2123fe82e8a3900eaef
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-06-29 23:46:51 +0500

    BOM-1842 py3.8 supported release. Bump the version.

--- bulk_grades/__init__.py
+++ bulk_grades/__init__.py
@@ -2,6 +2,6 @@
 Support for bulk scoring and grading.
 """
 
-__version__ = '0.6.9'
+__version__ = '0.7.0'
 
 default_app_config = 'bulk_grades.apps.BulkGradesConfig'  # pylint: disable=invalid-name
