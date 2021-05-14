a772436ea130a1010270cda98fce29119a4a8130
Nathan Sprenkle <nsprenkle@edx.org>
2021-05-14 11:55:58 -0400

    chore: bump bulk grades version

--- bulk_grades/__init__.py
+++ bulk_grades/__init__.py
@@ -2,6 +2,6 @@
 Support for bulk scoring and grading.
 """
 
-__version__ = '0.8.9'
+__version__ = '0.8.10'
 
 default_app_config = 'bulk_grades.apps.BulkGradesConfig'  # pylint: disable=invalid-name
